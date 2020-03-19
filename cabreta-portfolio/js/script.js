2224b3e0c666a19117daf24c246f60e4aca29a24
Auxence <auxence.blondel@gmail.com>
2020-03-19 22:10:09 +0100

    add sort gallery

--- cabreta-portfolio/js/script.js
+++ cabreta-portfolio/js/script.js
@@ -1,3 +1,5 @@
+
+// ---- DARKMODE ----
 const darkMod = document.getElementById("darkMode");
 const logoCabreta = document.getElementById("logo");
 const rootElt = document.documentElement;
@@ -20,6 +22,51 @@ darkMod.onclick = function(){
 rootElt.setAttribute('data-theme', sessionStorage.getItem("colorMod"));
 logoCabreta.src = sessionStorage.getItem("logo");
 
+// SORT PICTURES
+// const btnSort = document.getElementsByClassName('btn');
+// console.log(btnSort);
+
+const showAll = document.getElementById("showAll");
+const sport = document.getElementById("sport");
+const cooking = document.getElementById("cooking");
+const technology = document.getElementById("technology");
 
 
+function resetSort(){
+    showAll.style.display = "none";
+    sport.style.display = "none";
+    cooking.style.display = "none";
+    technology.style.display = "none";
+}
 
+function filterSelection(filter){
+    resetSort();
+    switch (filter) {
+        case 'all':
+            console.log('showall')
+            showAll.style.display = 'grid';
+            break;
+        case 'sport':
+            console.log('sport')
+            sport.style.display = 'grid';
+            break;
+        case 'cooking':
+            console.log('cooking')
+            cooking.style.display = 'grid';
+            break;
+        case 'technology':
+            console.log('technology')
+            technology.style.display = 'grid';
+            break;
+        default:
+            break;
+    }
+}
+let gallery = document.getElementById("showAll");
+// console.log(gallery);
+
+// for(let i = 0; i < gallery.length; i++ ){
+//     console.log(gallery[i].children[0]);
+//     gallery[i].children[0].style.borderRadius = "60px";
+// }
+// console.log(document.getElementById("showAll").children[0].children[0])
